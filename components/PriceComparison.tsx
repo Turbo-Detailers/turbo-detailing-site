e1a2efdda72bb50306c37b64f7810ec4adb7bd7d
Dhairya Gupta <dhairyag23@gmail.com>
2023-02-05 23:37:12 -0600

    Quick changes to the styling

--- components/PriceComparison.tsx
+++ components/PriceComparison.tsx
@@ -4,13 +4,13 @@ function PriceComparison() {
   return (
     <div className={styles.div}>
       <div className={styles.sideComparison}>
-        <div className={styles.titleText}>Basic</div>
+        <div className={styles.titleText}>Interior</div>
       </div>
       <div className={styles.centerComparison}>
         <div className={styles.titleText}>Turbo</div>
       </div>
       <div className={styles.sideComparison}>
-        <div className={styles.titleText}>Premium</div>
+        <div className={styles.titleText}>Exterior</div>
       </div>
     </div>
   );
