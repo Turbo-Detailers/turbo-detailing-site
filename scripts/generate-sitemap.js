d8340b33f5ba8e7225e88e7e58682094bfee717f
Dhairya Gupta <dhairyag23@gmail.com>
2023-02-08 16:49:36 -0600

    SITEMAPS

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -2,20 +2,26 @@ const fs = require("fs");
 const globby = require("globby");
 
 function addPage(page) {
-  const path = page.replace("pages", "").replace(".js", "").replace(".mdx", "");
+  const path = page
+    .replace("pages", "")
+    .replace(".js", "")
+    .replace(".mdx", "")
+    .replace(".tsx", "")
+    .replace(".ts", "");
   const route = path === "/index" ? "/" : path;
 
   return `  <url>
     <loc>${`${process.env.WEBSITE_URL}${route}`}</loc>
     <changefreq>hourly</changefreq>
+    <lastmod>2023-02-08</lastmod>
   </url>`;
 }
 
 async function generateSitemap() {
   // Ignore Next.js specific files (e.g., _app.js) and API routes.
   const pages = await globby([
-    "pages/**/*{.js,.mdx, .ts, .tsx}",
-    "!pages/_*.js",
+    "pages/**/*{.js,.mdx,.ts,.tsx}",
+    "!pages/_*{.js,.ts,.tsx}",
     "!pages/api",
   ]);
   const sitemap = `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
