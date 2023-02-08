270498c4d9249b82cca2e05ecf5a862593313828
Dhairya Gupta <dhairyag23@gmail.com>
2023-02-08 11:05:39 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require('./scripts/generate-sitemap')
+    }
+
+    return config
+  },
 }
 
 module.exports = nextConfig
