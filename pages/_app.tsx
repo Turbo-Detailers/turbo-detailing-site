8f4e5767425dc414a70fbc5db198f7e8ad61263a
Dhairya Gupta <dhairyag23@gmail.com>
2023-02-09 00:20:17 -0600

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import "../styles/globals.scss";
+import Head from "next/head";
 
 import type { AppProps } from "next/app";
 import Navbar from "../components/Navbar";
@@ -7,6 +8,10 @@ import { AnimatePresence } from "framer-motion";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main>
+<Head>
+<meta content-language="en-us"/> 
+<meta http-equiv="content-language" content="en-us"/>
+</Head>
       <Navbar />
       <AnimatePresence mode="wait">
         <Component {...pageProps} />
