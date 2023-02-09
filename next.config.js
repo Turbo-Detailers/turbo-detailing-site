a96d4f048567ff6dcdb8ce0e3357b1f303cf9c58
Dhairya Gupta <dhairyag23@gmail.com>
2023-02-09 00:24:07 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
   webpack: (config, { isServer }) => {
     if (isServer) {
       require("./scripts/generate-sitemap.js");
