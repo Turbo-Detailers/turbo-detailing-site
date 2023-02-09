027c0fd1ab42f42cdce705103bb58c2e197af002
Dhairya Gupta <dhairyag23@gmail.com>
2023-02-09 12:21:21 -0600

    cream

--- sections/Home/Top.tsx
+++ sections/Home/Top.tsx
@@ -1,14 +1,22 @@
+import { motion, useScroll } from "framer-motion";
 import AccentedTitle from "../../components/titles/AccentedTitle";
 import Subtitle from "../../components/titles/Subtitle";
 import styles from "../../styles/Home.module.scss";
 
 function Top() {
+  const { scrollYProgress } = useScroll();
+
   return (
     <main className={styles.main}>
       <div>
         <AccentedTitle>Turbo</AccentedTitle>
         <Subtitle>Luxury Detailing</Subtitle>
-        <p className={styles.scrollSign}>Scroll</p>
+        <motion.p
+          className={styles.scrollSign}
+          style={{ opacity: 1 / scrollYProgress }}
+        >
+          SCROLL <p className={styles.scrollSignBar}>━━━</p>
+        </motion.p>
       </div>
     </main>
   );
