318389a224d315a9b40f74a331ac19f0cb453280
Dhairya Gupta <dhairyag23@gmail.com>
2023-02-08 23:18:30 -0600

    new stuff

--- components/PriceComparison.tsx
+++ components/PriceComparison.tsx
@@ -1,21 +1,29 @@
 import { Fonts } from "../bin/fonts";
 import styles from "../styles/components/PriceComparison.module.scss";
 import pricingData from "../data/pricing.json";
+import Props from "../interfaces/Props";
+
+function PriceComparisonSection({ children }: Props) {
+  return (
+    <div className={styles.sideComparison}>
+      <div className={styles.sideTitleText}>{pricingData.interior.name}</div>
+      <div className={styles.benefits}>
+        {pricingData.interior.benefits.map((item) => (
+          <div key={item} className={styles.benefit}>
+            {item}
+          </div>
+        ))}
+      </div>
+      <div className={styles.price}>${pricingData.interior.price}</div>
+    </div>
+  );
+}
 
 function PriceComparison() {
   return (
     <div className={`${styles.div} ${Fonts.body}`}>
-      <div className={styles.sideComparison}>
-        <div className={styles.sideTitleText}>{pricingData.interior.name}</div>
-        <div className={styles.benefits}>
-          {pricingData.interior.benefits.map((item) => (
-            <div key={item} className={styles.benefit}>
-              {item}
-            </div>
-          ))}
-        </div>
-        <div className={styles.price}>${pricingData.interior.price}</div>
-      </div>
+      <PriceComparisonSection />
+
       <div className={styles.centerComparison}>
         <div className={styles.titleText}>{pricingData.full.name}</div>
         <div className={styles.benefits}>
@@ -28,7 +36,7 @@ function PriceComparison() {
         <div className={styles.price}>
           ${pricingData.full.price}
           <p>
-            <a className={styles.accent}>*</a> Most Popular
+            <a className={styles.accent}>*</a> {pricingData.full.note}
           </p>
         </div>
       </div>
