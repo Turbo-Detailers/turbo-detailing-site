b9f803ebbf6532e6540b892f15658d660c556d36
Dhairya Gupta <dhairyag23@gmail.com>
2023-02-09 22:08:15 -0600

    ok

--- sections/Home/Top.tsx
+++ sections/Home/Top.tsx
@@ -1,4 +1,4 @@
-import { motion, useScroll } from "framer-motion";
+import { motion, useScroll, useMotionValue, useTransform } from "framer-motion";
 import AccentedTitle from "../../components/titles/AccentedTitle";
 import Subtitle from "../../components/titles/Subtitle";
 import styles from "../../styles/Home.module.scss";
@@ -6,16 +6,18 @@ import styles from "../../styles/Home.module.scss";
 function Top() {
   const { scrollYProgress } = useScroll();
 
-  console.log(scrollYProgress);
+  const x = useMotionValue(0);
+
+  const input = [-200, 0, 200];
+  const output = [0, 1, 0];
+  const opacity = useTransform(x, input, output);
+
   return (
     <main className={styles.main}>
       <div>
         <AccentedTitle>Turbo</AccentedTitle>
         <Subtitle>Luxury Detailing</Subtitle>
-        <motion.p
-          className={styles.scrollSign}
-          // style={{ opacity: 1 - 0.01 * Number(scrollYProgress) }}
-        >
+        <motion.p className={styles.scrollSign} style={{ opacity }}>
           SCROLL <p className={styles.scrollSignBar}>━━━</p>
         </motion.p>
       </div>
