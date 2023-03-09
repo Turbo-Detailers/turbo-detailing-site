d1cbc5927c3e0dc1e515b8a0782a2b5e54707037
Dhairya Gupta <dhairyag23@gmail.com>
2023-03-09 10:47:02 -0600

    k

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,16 +7,20 @@ import { motion, AnimatePresence } from "framer-motion";
 import Footer from "../components/Footer";
 import Layout from "../components/Layout";
 
+import { GoogleAnalytics } from "nextjs-google-analytics";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main>
       <Head>
         <meta content-language="en-us" />
         <meta httpEquiv="content-language" content="en-us" />
+        <meta charSet="UTF-8" />
       </Head>
       <Navbar />
       <AnimatePresence mode="wait" initial={true}>
         <Layout>
+          <GoogleAnalytics trackPageViews gaMeasurementId="G-EC18NFWZEX" />
           <Component {...pageProps} />
         </Layout>
       </AnimatePresence>
