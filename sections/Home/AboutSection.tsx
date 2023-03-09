ff8994797d86b6965224dcda0a56a9c07d56d3fe
Dhairya Gupta <dhairyag23@gmail.com>
2023-03-09 11:11:16 -0600

    added picture

--- sections/Home/AboutSection.tsx
+++ sections/Home/AboutSection.tsx
@@ -1,46 +1,40 @@
 import { motion } from "framer-motion";
 import { Fonts } from "../../bin/fonts";
 import variants from "../../bin/variants";
-import sectionTwoStyles from "../../styles/components/Home/SectionTwo.module.scss";
+import styles from "../../styles/components/Home/AboutSection.module.scss";
 import Image from "next/image";
 
 function AboutSection() {
   return (
-    <div className={sectionTwoStyles.sectionTwo}>
+    <div className={styles.sectionTwo}>
       <motion.div
         variants={variants.fadeInUp}
         initial={"hidden"}
         whileInView={"show"}
         viewport={{ once: true }}
-        className={sectionTwoStyles.wrapper}
+        className={styles.wrapper}
       >
-        <div
-          style={{
-            borderRadius: "10px",
-            width: "100%",
-            position: "relative",
-            backgroundColor: "none",
-          }}
-        >
-          <div style={{ marginLeft: "2rem" }}>
-            <h1 className={`${sectionTwoStyles.title} ${Fonts.body}`}>
-              <b>About Us</b>
-            </h1>
-          </div>
-          <div className={`${sectionTwoStyles.paragraph} ${Fonts.body}`}>
-            Welcome to Turbo Luxury Auto Detailing, where we tailor our services
-            to meet the unique needs of your vehicle. Our full interior and
-            exterior detailing services use only the best products and
-            techniques to leave your car looking and feeling like new. Our team
-            of experts personalize their approach to fit the specific make and
-            model of your car, from leather and upholstery care to tire shining.
-            {/* <AccentedText> */} Experience the Turbo difference.
-            {/* </AccentedText> */}
+        <div className={"flex-row"}>
+          <div className={styles.textDiv}>
+            <div className={styles.title}>
+              <h1 className={`${styles.title} ${Fonts.body}`}>
+                <b>About Us</b>
+              </h1>
+            </div>
+            <div className={`${styles.paragraph} ${Fonts.body}`}>
+              Welcome to Turbo Luxury Auto Detailing, where we tailor our
+              services to meet the unique needs of your vehicle. Our full
+              interior and exterior detailing services use only the best
+              products and techniques to leave your car looking and feeling like
+              new. Our team of experts personalize their approach to fit the
+              specific make and model of your car, from leather and upholstery
+              care to tire shining. Experience the Turbo difference.
+            </div>
           </div>
           <Image
-            src={"/images/home/3.png"}
+            src={"/images/home/bugatti.JPG"}
             alt="Bugatti"
-            key="/images/home/3.png"
+            key="/images/home/bugatti.JPG"
             fill={true}
             quality={100}
             style={{ objectFit: "cover", zIndex: -2 }}
