147ea1703056b7702c1e48c71dbc0005863770ae
Dhairya Gupta <dhairyag23@gmail.com>
2023-03-29 15:35:04 -0500

    seo improvements

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,10 +16,23 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta content-language="en-us" />
         <meta httpEquiv="content-language" content="en-us" />
         <meta charSet="UTF-8" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:site_name" content="Turbo Detailers" />
+        <meta property="og:type" content="website" />
+        <meta
+          name="description"
+          content="Turbo Mobile Auto Detailing: Low price, high quality luxury detailing in the Twin Cities area."
+        />
+        <meta property="og:image" content="Turbo Detailers" />
       </Head>
       <Navbar />
       <AnimatePresence mode="wait" initial={true}>
         <Layout>
+          <div itemScope itemType="https://schema.org/WebSite">
+            <meta itemProp="url" content="https://turbodetailers.com/" />
+            <meta itemProp="name" content="Turbo Detailers" />
+            <meta itemProp="alternateName" content="Turbo Mobile Detailing" />
+          </div>
           <GoogleAnalytics trackPageViews gaMeasurementId="G-EC18NFWZEX" />
           <Component {...pageProps} />
         </Layout>
