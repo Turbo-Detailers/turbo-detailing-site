dd3b662eb2d1633dd6f1c2e0d5933227b02c9a40
Dhairya Gupta <dhairyag23@gmail.com>
2023-04-15 23:22:58 -0500

    add anayltics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,47 +9,52 @@ import Layout from "../components/Layout";
 
 import Router from "next/router";
 
+import { Analytics } from "@vercel/analytics/react";
+
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
-    <main>
-      <Head>
-        <meta content-language="en-us" />
-        <meta httpEquiv="content-language" content="en-us" />
-        <meta charSet="UTF-8" />
-        <meta property="og:locale" content="en_US" />
-        <meta property="og:site_name" content="Turbo Detailers" />
-        <meta property="og:type" content="website" />
-        <meta
-          name="description"
-          content="Turbo Mobile Auto Detailing: Low price, high quality luxury detailing in the Twin Cities area."
-        />
-        <meta
-          property="og:image"
-          content="/images/og/social-sharing-with-text.jpg"
-        />
-      </Head>
-      <Navbar />
-      <div itemScope itemType="https://schema.org/WebSite">
-        <meta itemProp="url" content="https://turbodetailers.com/" />
-        <meta itemProp="name" content="Turbo Detailers" />
-        <meta itemProp="alternateName" content="Turbo Mobile Detailing" />
-      </div>
-      <AnimatePresence mode="wait" initial={true}>
-        <Layout key={router.route}>
-          {/* <GoogleAnalytics trackPageViews gaMeasurementId="G-EC18NFWZEX" /> */}
-          {/* <motion.div
+    <>
+      <main>
+        <Head>
+          <meta content-language="en-us" />
+          <meta httpEquiv="content-language" content="en-us" />
+          <meta charSet="UTF-8" />
+          <meta property="og:locale" content="en_US" />
+          <meta property="og:site_name" content="Turbo Detailers" />
+          <meta property="og:type" content="website" />
+          <meta
+            name="description"
+            content="Turbo Mobile Auto Detailing: Low price, high quality luxury detailing in the Twin Cities area."
+          />
+          <meta
+            property="og:image"
+            content="/images/og/social-sharing-with-text.jpg"
+          />
+        </Head>
+        <Navbar />
+        <div itemScope itemType="https://schema.org/WebSite">
+          <meta itemProp="url" content="https://turbodetailers.com/" />
+          <meta itemProp="name" content="Turbo Detailers" />
+          <meta itemProp="alternateName" content="Turbo Mobile Detailing" />
+        </div>
+        <AnimatePresence mode="wait" initial={true}>
+          <Layout key={router.route}>
+            {/* <GoogleAnalytics trackPageViews gaMeasurementId="G-EC18NFWZEX" /> */}
+            {/* <motion.div
             key={router.route}
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             transition={{ opacity: { duration: 2 } }}
           > */}
-          <Component {...pageProps} />
-          {/* </motion.div> */}
-        </Layout>
-      </AnimatePresence>
-      <Footer />
-    </main>
+            <Component {...pageProps} />
+            {/* </motion.div> */}
+          </Layout>
+        </AnimatePresence>
+        <Footer />
+      </main>
+      <Analytics />
+    </>
   );
 }
 
