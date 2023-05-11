697da2619a842577d4c1b9ba1579030ecf8cffcf
Dhairya Gupta <dhairyag23@gmail.com>
2023-05-11 11:08:03 -0500

    footer update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,14 @@
+import Link from "next/link";
 import styles from "../styles/components/Footer.module.scss";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      &copy; 2023 Turbo Detailing. All rights reserved.
+      &copy; 2023{" "}
+      <Link href={"https://www.instagram.com/turbodetailersmn"}>
+        Turbo Detailing.
+      </Link>{" "}
+      All rights reserved.
     </footer>
   );
 }
