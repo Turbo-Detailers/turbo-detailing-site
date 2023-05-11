1bec9c62936448b15fcb50793c1138c601f26006
Dhairya Gupta <dhairyag23@gmail.com>
2023-05-11 10:27:24 -0500

    add x5 interior pictures

--- data/images.tsx
+++ data/images.tsx
@@ -6,14 +6,29 @@ import a4SteeringWheel from "../public/images/details/a4/a4-steering-wheel.jpeg"
 import a4InteriorDoorPanelSide from "../public/images/details/a4/a4-interior-door-panel-side.jpeg";
 
 // Tesla Model Y Imports
-import modelYFrontSoap from "../public/images/details/model-y/model-y-front-soap.jpg";
-import modelYFrontClean from "../public/images/details/model-y/model-y-front-clean.jpg";
-import modelYIsometricClean from "../public/images/details/model-y/model-y-isometric-clean.jpg";
-import modelYRearClean from "../public/images/details/model-y/model-y-rear-clean.jpg";
-import modelYSideRearClean from "../public/images/details/model-y/model-y-side-rear-clean.jpg";
-import modelYWheelClean from "../public/images/details/model-y/model-y-wheel-clean.jpg";
+import modelYFrontSoap from "../public/images/details/model-y-exterior/model-y-front-soap.jpg";
+import modelYFrontClean from "../public/images/details/model-y-exterior/model-y-front-clean.jpg";
+import modelYIsometricClean from "../public/images/details/model-y-exterior/model-y-isometric-clean.jpg";
+import modelYRearClean from "../public/images/details/model-y-exterior/model-y-rear-clean.jpg";
+import modelYSideRearClean from "../public/images/details/model-y-exterior/model-y-side-rear-clean.jpg";
+import modelYWheelClean from "../public/images/details/model-y-exterior/model-y-wheel-clean.jpg";
+
+// BMW X5 Imports
+import x5DoorPanelButtons from "../public/images/details/x5-interior/x5-door-panel-buttons.jpg";
+import x5DriverSideInstrumentCluster from "../public/images/details/x5-interior/x5-driver-side-instrument-cluster.jpg";
+import x5FrontSeatsRear from "../public/images/details/x5-interior/x5-front-seats-rear.jpg";
+import x5Indicator from "../public/images/details/x5-interior/x5-indicator.jpg";
+
+import { StaticImageData } from "next/image";
+
+type PhotoData = {
+  src: string;
+  width: number;
+  height: number;
+};
 
 export const localImages = [
+  x5DoorPanelButtons,
   modelYWheelClean,
   a4InteriorWheel,
   modelYFrontSoap,
@@ -26,12 +41,32 @@ export const localImages = [
   a4InteriorRear,
 ];
 
-export const photos = [
+function getPhotosObject(images: StaticImageData[]): PhotoData[] {
+  var photosObject: PhotoData[] = [];
+
+  for (var image in images) {
+    photosObject.push({
+      src: image.src,
+      width: image.width,
+      height: image.height,
+    });
+  }
+
+  return photosObject;
+}
+
+export const photos: PhotoData[] = [
+  {
+    src: x5DoorPanelButtons.src,
+    width: x5DoorPanelButtons.width,
+    height: x5DoorPanelButtons.height,
+  },
   {
     src: modelYWheelClean.src,
     width: modelYWheelClean.width,
     height: modelYWheelClean.height,
   },
+
   {
     src: a4InteriorWheel.src,
     width: a4InteriorWheel.width,
@@ -41,6 +76,11 @@ export const photos = [
     src: modelYSideRearClean.src,
     width: modelYSideRearClean.width,
     height: modelYSideRearClean.height,
+  },
+  {
+    src: x5FrontSeatsRear.src,
+    width: x5FrontSeatsRear.width,
+    height: x5FrontSeatsRear.height,
   },
   {
     src: a4InteriorDoorPanel.src,
@@ -63,6 +103,11 @@ export const photos = [
     height: modelYIsometricClean.height,
   },
   {
+    src: x5Indicator.src,
+    width: x5Indicator.width,
+    height: x5Indicator.height,
+  },
+  {
     src: a4InteriorDoorPanelSide.src,
     width: a4InteriorDoorPanelSide.width,
     height: a4InteriorDoorPanelSide.height,
@@ -83,5 +128,10 @@ export const photos = [
     src: a4InteriorRear.src,
     width: a4InteriorRear.width,
     height: a4InteriorRear.height,
+  },
+  {
+    src: x5DriverSideInstrumentCluster.src,
+    width: x5DriverSideInstrumentCluster.width,
+    height: x5DriverSideInstrumentCluster.height,
   },
 ];
