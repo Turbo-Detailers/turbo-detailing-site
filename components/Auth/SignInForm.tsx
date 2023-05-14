23ecce95024a03395eac59901539ff35491028a7
Dhairya Gupta <dhairyag23@gmail.com>
2023-05-14 00:47:11 -0500

    wip

--- components/Auth/SignInForm.tsx
+++ components/Auth/SignInForm.tsx
@@ -1,84 +1,84 @@
-import { useState } from "react";
-import styles from "../../styles/components/AuthForms.module.scss";
-import {
-  getAuth,
-  sendPasswordResetEmail,
-  signInWithEmailAndPassword,
-} from "firebase/auth";
+// import { useState } from "react";
+// import styles from "../../styles/components/AuthForms.module.scss";
+// import {
+//   getAuth,
+//   sendPasswordResetEmail,
+//   signInWithEmailAndPassword,
+// } from "firebase/auth";
 
-import { ToastContainer, toast } from "react-toastify";
-import { FirebaseError } from "firebase/app";
+// import { ToastContainer, toast } from "react-toastify";
+// import { FirebaseError } from "firebase/app";
 
-import "react-toastify/dist/ReactToastify.css";
-import Head from "next/head";
+// import "react-toastify/dist/ReactToastify.css";
+// import Head from "next/head";
 
-export default function SignInForm() {
-  const auth = getAuth();
-  // Input Change Handling
-  const [inputs, setInputs] = useState({
-    email: "",
-    password: "",
-  });
-  const handleOnChange = (event: any) => {
-    event.persist();
-    setInputs((prev) => ({
-      ...prev,
-      [event.target.id]: event.target.value,
-    }));
-  };
+// export default function SignInForm() {
+//   const auth = getAuth();
+//   // Input Change Handling
+//   const [inputs, setInputs] = useState({
+//     email: "",
+//     password: "",
+//   });
+//   const handleOnChange = (event: any) => {
+//     event.persist();
+//     setInputs((prev) => ({
+//       ...prev,
+//       [event.target.id]: event.target.value,
+//     }));
+//   };
 
-  // Auth State Handling
-  const handleOnSubmit = (event: any) => {
-    event.preventDefault();
+//   // Auth State Handling
+//   const handleOnSubmit = (event: any) => {
+//     event.preventDefault();
 
-    if (inputs.email === "" || inputs.password === "") return;
+//     if (inputs.email === "" || inputs.password === "") return;
 
-    signInWithEmailAndPassword(auth, inputs.email, inputs.password).catch(
-      (e: FirebaseError) =>
-        toast.error(e.code, { theme: "colored", position: "bottom-right" })
-    );
-  };
+//     signInWithEmailAndPassword(auth, inputs.email, inputs.password).catch(
+//       (e: FirebaseError) =>
+//         toast.error(e.code, { theme: "colored", position: "bottom-right" })
+//     );
+//   };
 
-  return (
-    <>
-      <div className={styles.page}>
-        <form onSubmit={handleOnSubmit} className={styles.form}>
-          <input
-            onChange={handleOnChange}
-            value={inputs.email}
-            id="email"
-            type="email"
-            name="email"
-            placeholder="Email"
-          />
-          <input
-            onChange={handleOnChange}
-            value={inputs.password}
-            id="password"
-            type="password"
-            name="password"
-            placeholder="Password"
-          />
+//   return (
+//     <>
+//       <div className={styles.page}>
+//         <form onSubmit={handleOnSubmit} className={styles.form}>
+//           <input
+//             onChange={handleOnChange}
+//             value={inputs.email}
+//             id="email"
+//             type="email"
+//             name="email"
+//             placeholder="Email"
+//           />
+//           <input
+//             onChange={handleOnChange}
+//             value={inputs.password}
+//             id="password"
+//             type="password"
+//             name="password"
+//             placeholder="Password"
+//           />
 
-          <div className={styles.forgotPasswordContainer}>
-            <a
-              onClick={() =>
-                sendPasswordResetEmail(auth, inputs.email).catch(
-                  (e: FirebaseError) =>
-                    toast.error(e.code, {
-                      theme: "colored",
-                      position: "bottom-right",
-                    })
-                )
-              }
-            >
-              Forgot Password?
-            </a>
-          </div>
-          <button type="submit"> Sign In </button>
-        </form>
-        <ToastContainer />
-      </div>
-    </>
-  );
-}
+//           <div className={styles.forgotPasswordContainer}>
+//             <a
+//               onClick={() =>
+//                 sendPasswordResetEmail(auth, inputs.email).catch(
+//                   (e: FirebaseError) =>
+//                     toast.error(e.code, {
+//                       theme: "colored",
+//                       position: "bottom-right",
+//                     })
+//                 )
+//               }
+//             >
+//               Forgot Password?
+//             </a>
+//           </div>
+//           <button type="submit"> Sign In </button>
+//         </form>
+//         <ToastContainer />
+//       </div>
+//     </>
+//   );
+// }
--- components/Auth/SignUpForm.tsx
+++ components/Auth/SignUpForm.tsx
@@ -1,67 +1,67 @@
-import { useState } from "react";
-import styles from "../../styles/components/AuthForms.module.scss";
-import {
-  createUserWithEmailAndPassword,
-  getAuth,
-  sendPasswordResetEmail,
-  signInWithEmailAndPassword,
-} from "firebase/auth";
+// import { useState } from "react";
+// import styles from "../../styles/components/AuthForms.module.scss";
+// import {
+//   createUserWithEmailAndPassword,
+//   getAuth,
+//   sendPasswordResetEmail,
+//   signInWithEmailAndPassword,
+// } from "firebase/auth";
 
-import { ToastContainer, toast } from "react-toastify";
-import { FirebaseError } from "firebase/app";
+// import { ToastContainer, toast } from "react-toastify";
+// import { FirebaseError } from "firebase/app";
 
-import "react-toastify/dist/ReactToastify.css";
+// import "react-toastify/dist/ReactToastify.css";
 
-export default function SignUpForm() {
-  const auth = getAuth();
-  // Input Change Handling
-  const [inputs, setInputs] = useState({
-    email: "",
-    password: "",
-  });
-  const handleOnChange = (event: any) => {
-    event.persist();
-    setInputs((prev) => ({
-      ...prev,
-      [event.target.id]: event.target.value,
-    }));
-  };
+// export default function SignUpForm() {
+//   const auth = getAuth();
+//   // Input Change Handling
+//   const [inputs, setInputs] = useState({
+//     email: "",
+//     password: "",
+//   });
+//   const handleOnChange = (event: any) => {
+//     event.persist();
+//     setInputs((prev) => ({
+//       ...prev,
+//       [event.target.id]: event.target.value,
+//     }));
+//   };
 
-  // Auth State Handling
-  const handleOnSubmit = (event: any) => {
-    event.preventDefault();
+//   // Auth State Handling
+//   const handleOnSubmit = (event: any) => {
+//     event.preventDefault();
 
-    if (inputs.email === "" || inputs.password === "") return;
+//     if (inputs.email === "" || inputs.password === "") return;
 
-    createUserWithEmailAndPassword(auth, inputs.email, inputs.password).catch(
-      (e: FirebaseError) =>
-        toast.error(e.code, { theme: "colored", position: "bottom-right" })
-    );
-  };
+//     createUserWithEmailAndPassword(auth, inputs.email, inputs.password).catch(
+//       (e: FirebaseError) =>
+//         toast.error(e.code, { theme: "colored", position: "bottom-right" })
+//     );
+//   };
 
-  return (
-    <div className={styles.page}>
-      <form onSubmit={handleOnSubmit} className={styles.form}>
-        <input
-          onChange={handleOnChange}
-          value={inputs.email}
-          id="email"
-          type="email"
-          name="email"
-          placeholder="Email"
-        />
-        <input
-          onChange={handleOnChange}
-          value={inputs.password}
-          id="password"
-          type="password"
-          name="password"
-          placeholder="Password"
-        />
+//   return (
+//     <div className={styles.page}>
+//       <form onSubmit={handleOnSubmit} className={styles.form}>
+//         <input
+//           onChange={handleOnChange}
+//           value={inputs.email}
+//           id="email"
+//           type="email"
+//           name="email"
+//           placeholder="Email"
+//         />
+//         <input
+//           onChange={handleOnChange}
+//           value={inputs.password}
+//           id="password"
+//           type="password"
+//           name="password"
+//           placeholder="Password"
+//         />
 
-        <button type="submit"> Sign Up </button>
-      </form>
-      <ToastContainer />
-    </div>
-  );
-}
+//         <button type="submit"> Sign Up </button>
+//       </form>
+//       <ToastContainer />
+//     </div>
+//   );
+// }
