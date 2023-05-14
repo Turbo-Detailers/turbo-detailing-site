b9aef289ab15bd350e8c51fc44f65d463d19af2c
Dhairya Gupta <dhairyag23@gmail.com>
2023-05-13 23:40:37 -0500

    Update [...nextauth].ts

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -40,12 +40,12 @@ export const authOptions: AuthOptions = {
   // },
 };
 
-export const firestore = initFirestore({
-  credential: cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/gm, "\n"),
-  }),
-});
+// export const firestore = initFirestore({
+//   credential: cert({
+//     projectId: process.env.FIREBASE_PROJECT_ID,
+//     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+//     privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/gm, "\n"),
+//   }),
+// });
 
 export default NextAuth(authOptions);
