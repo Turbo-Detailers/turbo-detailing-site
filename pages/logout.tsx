6b2df128b9a358fa2a69d00e85f33b4951eda45d
Dhairya Gupta <dhairyag23@gmail.com>
2023-05-14 01:12:28 -0500

    Update logout.tsx

--- pages/logout.tsx
+++ pages/logout.tsx
@@ -1,7 +1,12 @@
-import { signOut } from "next-auth/react";
+import { signOut, useSession, signIn } from "next-auth/react";
 
 export default function LogoutPage() {
+const { data: session } = useSession();
+  if (session !== undefined && session !== null) {
   signOut();
+} else {
+signIn()
+}
 
   return <div>Logging you out...</div>;
 }
