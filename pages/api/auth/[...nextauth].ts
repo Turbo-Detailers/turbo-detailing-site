fc00ab73cf9df05faa2c53721851123a483cf3f8
Dhairya Gupta <dhairyag23@gmail.com>
2023-05-13 23:56:05 -0500

    test

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -24,28 +24,32 @@ export const authOptions: AuthOptions = {
     }),
     // ...add more providers here
   ],
-  // adapter: FirestoreAdapter({
-  //   credential: cert({
-  //     projectId: process.env.FIREBASE_PROJECT_ID || "",
-  //     clientEmail: process.env.FIREBASE_CLIENT_EMAIL || "",
-  //     privateKey:
-  //       process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/gm, "\n") || "",
-  //   }),
-  // }),
-  // callbacks: {
-  //   session: async ({ session, user }): Promise<Session> => {
-  //     session.user.id = user.id;
-  //     return Promise.resolve(session);
-  //   },
-  // },
+  adapter: FirestoreAdapter({
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID || "",
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL || "",
+      privateKey:
+        process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/gm, "\n") || "",
+    }),
+  }),
+  callbacks: {
+    session: async ({ session, user }): Promise<Session> => {
+      session.user.id = user.id;
+      return Promise.resolve(session);
+    },
+    jwt: async ({ token }) => {
+      token.userRole = "admin";
+      return token;
+    },
+  },
 };
 
-// export const firestore = initFirestore({
-//   credential: cert({
-//     projectId: process.env.FIREBASE_PROJECT_ID,
-//     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-//     privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/gm, "\n"),
-//   }),
-// });
+export const firestore = initFirestore({
+  credential: cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/gm, "\n"),
+  }),
+});
 
 export default NextAuth(authOptions);
--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -11,3 +11,14 @@ declare module "next-auth" {
     } & DefaultSession["user"];
   }
 }
+
+import "next-auth/jwt";
+
+// Read more at: https://next-auth.js.org/getting-started/typescript#module-augmentation
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    /** The user's role. */
+    userRole?: "admin" | "user" | "maintenance";
+  }
+}
