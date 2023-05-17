c3b5bb0dd8bf47d1c4fe0a9c67df05ff1eb76704
Dhairya Gupta <dhairyag23@gmail.com>
2023-05-16 22:29:34 -0500

    shuffle array

--- data/images.tsx
+++ data/images.tsx
@@ -65,7 +65,27 @@ function getPhotosObject(images: StaticImageData[]): PhotoData[] {
   return photosObject;
 }
 
-export const photos = getPhotosObject(localImages);
+function shuffle(array: any[]) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}
+
+export const photos = shuffle(getPhotosObject(localImages));
 
 // export const photos: PhotoData[] = [
 //   {
