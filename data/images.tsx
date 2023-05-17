bcff7a42d3f4e223a7c547dc7e67e44c122b0f45
Dhairya Gupta <dhairyag23@gmail.com>
2023-05-16 22:13:40 -0500

    make it auto generate the long list of images

--- data/images.tsx
+++ data/images.tsx
@@ -51,100 +51,102 @@ export const localImages = [
   mdx2023BroadDashboard,
 ];
 
-// function getPhotosObject(images: StaticImageData[]): PhotoData[] {
-//   var photosObject: PhotoData[] = [];
+function getPhotosObject(images: StaticImageData[]): PhotoData[] {
+  var photosObject: PhotoData[] = [];
 
-//   for (var image in images) {
-//     photosObject.push({
-//       src: image.src,
-//       width: image.width,
-//       height: image.height,
-//     });
-//   }
+  for (var i = 0; i < images.length; i++) {
+    photosObject.push({
+      src: images[i].src,
+      width: images[i].width,
+      height: images[i].height,
+    });
+  }
 
-//   return photosObject;
-// }
+  return photosObject;
+}
 
-export const photos: PhotoData[] = [
-  {
-    src: x5DoorPanelButtons.src,
-    width: x5DoorPanelButtons.width,
-    height: x5DoorPanelButtons.height,
-  },
-  {
-    src: modelYWheelClean.src,
-    width: modelYWheelClean.width,
-    height: modelYWheelClean.height,
-  },
+export const photos = getPhotosObject(localImages);
 
-  {
-    src: a4InteriorWheel.src,
-    width: a4InteriorWheel.width,
-    height: a4InteriorWheel.height,
-  },
-  {
-    src: x5FrontSeatsRear.src,
-    width: x5FrontSeatsRear.width,
-    height: x5FrontSeatsRear.height,
-  },
-  {
-    src: modelYSideRearClean.src,
-    width: modelYSideRearClean.width,
-    height: modelYSideRearClean.height,
-  },
+// export const photos: PhotoData[] = [
+//   {
+//     src: x5DoorPanelButtons.src,
+//     width: x5DoorPanelButtons.width,
+//     height: x5DoorPanelButtons.height,
+//   },
+//   {
+//     src: modelYWheelClean.src,
+//     width: modelYWheelClean.width,
+//     height: modelYWheelClean.height,
+//   },
 
-  {
-    src: x5Indicator.src,
-    width: x5Indicator.width,
-    height: x5Indicator.height,
-  },
-  {
-    src: a4InteriorDoorPanel.src,
-    width: a4InteriorDoorPanel.width,
-    height: a4InteriorDoorPanel.height,
-  },
+//   {
+//     src: a4InteriorWheel.src,
+//     width: a4InteriorWheel.width,
+//     height: a4InteriorWheel.height,
+//   },
+//   {
+//     src: x5FrontSeatsRear.src,
+//     width: x5FrontSeatsRear.width,
+//     height: x5FrontSeatsRear.height,
+//   },
+//   {
+//     src: modelYSideRearClean.src,
+//     width: modelYSideRearClean.width,
+//     height: modelYSideRearClean.height,
+//   },
 
-  {
-    src: modelYFrontClean.src,
-    width: modelYFrontClean.width,
-    height: modelYFrontClean.height,
-  },
-  {
-    src: a4SteeringWheel.src,
-    width: a4SteeringWheel.width,
-    height: a4SteeringWheel.height,
-  },
-  {
-    src: modelYIsometricClean.src,
-    width: modelYIsometricClean.width,
-    height: modelYIsometricClean.height,
-  },
+//   {
+//     src: x5Indicator.src,
+//     width: x5Indicator.width,
+//     height: x5Indicator.height,
+//   },
+//   {
+//     src: a4InteriorDoorPanel.src,
+//     width: a4InteriorDoorPanel.width,
+//     height: a4InteriorDoorPanel.height,
+//   },
 
-  {
-    src: a4InteriorDoorPanelSide.src,
-    width: a4InteriorDoorPanelSide.width,
-    height: a4InteriorDoorPanelSide.height,
-  },
-  {
-    src: modelYFrontSoap.src,
-    width: modelYFrontSoap.width,
-    height: modelYFrontSoap.height,
-  },
+//   {
+//     src: modelYFrontClean.src,
+//     width: modelYFrontClean.width,
+//     height: modelYFrontClean.height,
+//   },
+//   {
+//     src: a4SteeringWheel.src,
+//     width: a4SteeringWheel.width,
+//     height: a4SteeringWheel.height,
+//   },
+//   {
+//     src: modelYIsometricClean.src,
+//     width: modelYIsometricClean.width,
+//     height: modelYIsometricClean.height,
+//   },
 
-  {
-    src: modelYRearClean.src,
-    width: modelYRearClean.width,
-    height: modelYRearClean.height,
-  },
+//   {
+//     src: a4InteriorDoorPanelSide.src,
+//     width: a4InteriorDoorPanelSide.width,
+//     height: a4InteriorDoorPanelSide.height,
+//   },
+//   {
+//     src: modelYFrontSoap.src,
+//     width: modelYFrontSoap.width,
+//     height: modelYFrontSoap.height,
+//   },
 
-  {
-    src: a4InteriorRear.src,
-    width: a4InteriorRear.width,
-    height: a4InteriorRear.height,
-  },
-  {
-    src: x5DriverSideInstrumentCluster.src,
-    width: x5DriverSideInstrumentCluster.width,
-    height: x5DriverSideInstrumentCluster.height,
-  },
-];
+//   {
+//     src: modelYRearClean.src,
+//     width: modelYRearClean.width,
+//     height: modelYRearClean.height,
+//   },
+
+//   {
+//     src: a4InteriorRear.src,
+//     width: a4InteriorRear.width,
+//     height: a4InteriorRear.height,
+//   },
+//   {
+//     src: x5DriverSideInstrumentCluster.src,
+//     width: x5DriverSideInstrumentCluster.width,
+//     height: x5DriverSideInstrumentCluster.height,
+//   },
+// ];
