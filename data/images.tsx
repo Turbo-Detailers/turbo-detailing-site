04ba48754957ec5b6a111395861d85f8369c17d3
Dhairya Gupta <dhairyag23@gmail.com>
2023-05-20 17:18:52 -0500

    clean up

--- data/images.tsx
+++ data/images.tsx
@@ -115,11 +115,7 @@ function randomizeAmount(
   amount: number
 ): StaticImageData[] {
   images = shuffle(images);
-  var returnArray: StaticImageData[] = [];
-  for (var i = 0; i <= amount; i++) {
-    returnArray.push(images[i]);
-  }
-  return returnArray;
+  return images.splice(amount, images.length - amount);
 }
 
 export const photos = shuffle(getPhotosObject(localImages));
