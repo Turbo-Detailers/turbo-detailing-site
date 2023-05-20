dc846b4be9105d412dcb3beed4d1990c3271f725
Dhairya Gupta <dhairyag23@gmail.com>
2023-05-20 13:03:50 -0500

    possible fix

--- next.config.js
+++ next.config.js
@@ -4,10 +4,6 @@ const withImages = require("next-images");
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
   webpack: (config, { isServer }) => {
     if (isServer) {
       require("./scripts/generate-sitemap.js");
