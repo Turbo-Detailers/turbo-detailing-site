be13217a5aae6fc8f2b81e152d8a238619c3a75b
Dhairya Gupta <dhairyag23@gmail.com>
2023-05-25 11:04:49 -0500

    mdx

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -3,7 +3,7 @@ import { defineDocumentType, makeSource } from "@contentlayer/source-files";
 
 export const Post = defineDocumentType(() => ({
   name: "Post",
-  filePathPattern: `**/*.md`,
+  filePathPattern: `**/*.mdx`,
   // contentType: "mdx",
   fields: {
     title: { type: "string", required: true },
