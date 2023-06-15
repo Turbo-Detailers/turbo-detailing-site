4c517ed5dfd2df36ada5124bfc0b05f3f86b16ee
Dhairya Gupta <dhairyag23@gmail.com>
2023-06-15 01:48:43 -0500

    hyoe

--- components/Brands.tsx
+++ components/Brands.tsx
@@ -1,7 +1,16 @@
 export default function Brands() {
   return (
-    <div>
-      <img src="/images/brands/lm.png" style={{ filter: "invert()" }}></img>
+    <div style={{ width: "100%", display: "flex" }}>
+      <img
+        loading="lazy"
+        src="/images/brands/lm.png"
+        style={{ filter: "invert()" }}
+      ></img>
+      <img
+        loading="lazy"
+        src="/images/brands/carpro.png"
+        style={{ filter: "invert()", maxWidth: "30%", objectFit: "contain" }}
+      ></img>
     </div>
   );
 }
