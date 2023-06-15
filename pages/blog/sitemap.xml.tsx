7a4ba4aa35395250afde9b3c928c6e6028805637
Dhairya Gupta <dhairyag23@gmail.com>
2023-06-15 05:13:00 -0500

    Update sitemap.xml.tsx

--- pages/blog/sitemap.xml.tsx
+++ pages/blog/sitemap.xml.tsx
@@ -37,7 +37,6 @@ async function generateSitemap() {
   // https://www.sitemaps.org/protocol.html
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url><loc>${WEBSITE_URL}</loc></url>
     ${allPosts
       .map(
         ({ url, date }) => `<url>
