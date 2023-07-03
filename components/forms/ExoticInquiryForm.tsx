3d0c8069bacc8decca3d4475cf42b8395c2a1327
Dhairya Gupta <dhairyag23@gmail.com>
2023-07-03 16:34:15 -0500

    bet bet bet

--- components/forms/ExoticInquiryForm.tsx
+++ components/forms/ExoticInquiryForm.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useState } from "react";
-import styles from "../../styles/components/QuoteForm.module.scss";
+import { ToastContainer, toast } from "react-toastify";
+
 export default function ContactForm() {
   var [submitted, setSubmitted] = useState(false);
   // Input Change Handling
@@ -47,52 +48,114 @@ export default function ContactForm() {
       })
       .catch((error: any) => {
         console.log("error");
+        toast(`Error: ${error.error}`);
       });
   };
 
   if (submitted)
     return (
-      <div className={styles.form}>
-        <h3>Message Sent!</h3>
-      </div>
+      <section className="bg-gray-900">
+        <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-white">
+            Exotic Vehicles
+          </h2>
+          <p className="mb-8 lg:mb-16 font-light text-center text-gray-400 sm:text-xl">
+            Your message was sent successfully! Check your email for
+            confirmation.
+          </p>
+        </div>
+        <ToastContainer />
+      </section>
     );
   return (
-    <div className={styles.container}>
-      <form onSubmit={handleOnSubmit} className={styles.form}>
-        <input
-          onChange={handleOnChange}
-          value={inputs.name}
-          id="name"
-          type="text"
-          name="name"
-          placeholder="Name"
-        />
-        <input
-          onChange={handleOnChange}
-          value={inputs.email}
-          id="email"
-          type="email"
-          name="email"
-          placeholder="Email"
-        />
-        <input
-          onChange={handleOnChange}
-          value={inputs.number}
-          id="number"
-          type="tel"
-          name="number"
-          placeholder="Phone Number"
-        />
+    <section className="bg-gray-900">
+      <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-white">
+          Exotic Vehicles
+        </h2>
+        <p className="mb-8 lg:mb-16 font-light text-center text-gray-400 sm:text-xl">
+          Get personalized service that works with your schedule. Let us know
+          what you'd like below.
+        </p>
+        <form onSubmit={handleOnSubmit} className="space-y-8">
+          <div>
+            <label
+              htmlFor="name"
+              className="block mb-2 text-sm font-medium text-gray-300"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="shadow-sm border border-gray-300 text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-primary-500 focus:border-primary-500 shadow-sm-light"
+              placeholder="Johnny Appleseed"
+              value={inputs.name}
+              onChange={handleOnChange}
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="email"
+              className="block mb-2 text-sm font-medium text-gray-300"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="shadow-sm border border-gray-300 text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-primary-500 focus:border-primary-500 shadow-sm-light"
+              placeholder="contact@turbodetailers.com"
+              value={inputs.email}
+              onChange={handleOnChange}
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="number"
+              className="block mb-2 text-sm font-medium text-gray-300"
+            >
+              Number
+            </label>
+            <input
+              type="tel"
+              id="number"
+              className="shadow-sm border border-gray-300 text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-primary-500 focus:border-primary-500 shadow-sm-light"
+              placeholder="(952) 457-5638"
+              value={inputs.number}
+              onChange={handleOnChange}
+              required
+            />
+          </div>
 
-        <textarea
-          onChange={handleOnChange}
-          value={inputs.notes}
-          id="notes"
-          name="notes"
-          placeholder="Please describe what vehicles you would like us to detail, and any specifics about the type of jobs and frequency."
-        />
-        <button type="submit"> Send Message </button>
-      </form>
-    </div>
+          <div className="sm:col-span-2">
+            <label
+              htmlFor="notes"
+              className="block mb-2 text-sm font-medium text-gray-300"
+            >
+              Message
+            </label>
+            <textarea
+              id="notes"
+              rows={6}
+              className="shadow-sm border border-gray-300 text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-primary-500 focus:border-primary-500 shadow-sm-light"
+              placeholder="Please describe what vehicles you would like us to detail, and any specifics about the type of jobs and frequency. Please also list any other requirements you may have."
+              value={inputs.notes}
+              required
+              onChange={handleOnChange}
+            ></textarea>
+          </div>
+          <button
+            type="submit"
+            className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-red-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+          >
+            Send message
+          </button>
+        </form>
+      </div>
+      <ToastContainer />
+    </section>
   );
 }
