5c8a70c403cfb509e2c8fa3bc8573d310d3f2411
Dhairya Gupta <dhairyag23@gmail.com>
2023-07-06 13:08:32 -0500

    add more images to exotics

--- data/images.tsx
+++ data/images.tsx
@@ -60,11 +60,16 @@ import velar9 from "details/images/velar/DSC_0723.jpg";
 import svj1 from "exotics/lamborghini-svj/svj-interior.jpg";
 import svj2 from "exotics/lamborghini-svj/svj-logo.jpg";
 import svj3 from "exotics/lamborghini-svj/svj-seat.jpg";
+import svj4 from "exotics/lamborghini-svj/svj-exhaust.jpg";
 
 // Lamborghini Aventador Roaster S
 import roadster1 from "exotics/lamborghini-roadster-s/roadster.jpg";
+import roadster2 from "exotics/lamborghini-roadster-s/roadster-side.jpg";
+import roadster3 from "exotics/lamborghini-roadster-s/roaster-tail.jpg";
 
 // Lamborghini Tractor
+import tractor1 from "exotics/lamborghini-tractor/tractor.jpg";
+import tractor2 from "exotics/lamborghini-tractor/tractor-1r.jpg";
 
 import { StaticImageData } from "next/image";
 
@@ -113,7 +118,17 @@ export const localImages: StaticImageData[] = [
   ...randomizeAmount(velarImages, 5),
 ];
 
-export const exoticImages: StaticImageData[] = [svj2, svj1, svj3, roadster1];
+export const exoticImages: StaticImageData[] = [
+  roadster1,
+  svj2,
+  tractor1,
+  svj1,
+  roadster2,
+  svj3,
+  tractor2,
+  roadster3,
+  svj4,
+];
 
 function getPhotosObject(images: StaticImageData[]): PhotoData[] {
   var photosObject: PhotoData[] = [];
@@ -162,87 +177,3 @@ export const photos = shuffle(getPhotosObject(localImages));
 export function getShuffledPhotos(): StaticImageData[] {
   return shuffle(getPhotosObject(localImages));
 }
-
-// export const photos: PhotoData[] = [
-//   {
-//     src: x5DoorPanelButtons.src,
-//     width: x5DoorPanelButtons.width,
-//     height: x5DoorPanelButtons.height,
-//   },
-//   {
-//     src: modelYWheelClean.src,
-//     width: modelYWheelClean.width,
-//     height: modelYWheelClean.height,
-//   },
-
-//   {
-//     src: a4InteriorWheel.src,
-//     width: a4InteriorWheel.width,
-//     height: a4InteriorWheel.height,
-//   },
-//   {
-//     src: x5FrontSeatsRear.src,
-//     width: x5FrontSeatsRear.width,
-//     height: x5FrontSeatsRear.height,
-//   },
-//   {
-//     src: modelYSideRearClean.src,
-//     width: modelYSideRearClean.width,
-//     height: modelYSideRearClean.height,
-//   },
-
-//   {
-//     src: x5Indicator.src,
-//     width: x5Indicator.width,
-//     height: x5Indicator.height,
-//   },
-//   {
-//     src: a4InteriorDoorPanel.src,
-//     width: a4InteriorDoorPanel.width,
-//     height: a4InteriorDoorPanel.height,
-//   },
-
-//   {
-//     src: modelYFrontClean.src,
-//     width: modelYFrontClean.width,
-//     height: modelYFrontClean.height,
-//   },
-//   {
-//     src: a4SteeringWheel.src,
-//     width: a4SteeringWheel.width,
-//     height: a4SteeringWheel.height,
-//   },
-//   {
-//     src: modelYIsometricClean.src,
-//     width: modelYIsometricClean.width,
-//     height: modelYIsometricClean.height,
-//   },
-
-//   {
-//     src: a4InteriorDoorPanelSide.src,
-//     width: a4InteriorDoorPanelSide.width,
-//     height: a4InteriorDoorPanelSide.height,
-//   },
-//   {
-//     src: modelYFrontSoap.src,
-//     width: modelYFrontSoap.width,
-//     height: modelYFrontSoap.height,
-//   },
-
-//   {
-//     src: modelYRearClean.src,
-//     width: modelYRearClean.width,
-//     height: modelYRearClean.height,
-//   },
-
-//   {
-//     src: a4InteriorRear.src,
-//     width: a4InteriorRear.width,
-//     height: a4InteriorRear.height,
-//   },
-//   {
-//     src: x5DriverSideInstrumentCluster.src,
-//     width: x5DriverSideInstrumentCluster.width,
-//     height: x5DriverSideInstrumentCluster.height,
-//   },
-// ];
