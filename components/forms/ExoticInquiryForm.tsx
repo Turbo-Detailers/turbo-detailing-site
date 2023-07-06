13f70e6b386df354ac8860b86cf9ad1e7b9e26bb
Dhairya Gupta <dhairyag23@gmail.com>
2023-07-06 01:47:38 -0500

    enzoo

--- components/forms/ExoticInquiryForm.tsx
+++ components/forms/ExoticInquiryForm.tsx
@@ -89,7 +89,7 @@ export default function ContactForm() {
               type="text"
               id="name"
               className="shadow-sm border border-gray-300 text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-primary-500 focus:border-primary-500 shadow-sm-light"
-              placeholder="Johnny Appleseed"
+              placeholder="Enzo Ferrari"
               value={inputs.name}
               onChange={handleOnChange}
               required
